8cc76bb1677087ca5497a01c2f81540e49765f51
Arezar <62929631+Arezar@users.noreply.github.com>
2026-03-05 21:31:52 +0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,54 +1,42 @@
-(function() {
+(function () {
     'use strict';
-    console.log('[AdBlock] Инициализация...');
 
-    // ============================================
-    // 1. Ждём загрузки Lampa
-    // ============================================
-    function onLampaReady(callback) {
-        if (window.Lampa) return callback();
-        var timer = setInterval(function() {
-            if (window.Lampa) {
-                clearInterval(timer);
-                callback();
-            }
-        }, 100);
+
+window.Account = window.Account || {};
+window.Account.hasPremium = () => true;
+
+
+document.createElement = new Proxy(document.createElement, {
+apply(target, thisArg, args) {
+if (args[0] === "video") {
+
+        let fakVideo = target.apply(thisArg, args);
+
+       
+        fakVideo.play = function () {
+            setTimeout(() => {
+                fakVideo.ended = true;
+                fakVideo.dispatchEvent(new Event("ended")); 
+            }, 500);
+        };
+
+        return fakVideo;
     }
+    return target.apply(thisArg, args);
+}
 
-    onLampaReady(function() {
+});
 
-        // ============================================
-        // 2. Блокируем рекламный модуль Lampa
-        // ============================================
-        if (Lampa.Ad) {
-            Lampa.Ad.show  = function() { console.log('[AdBlock] Ad.show заблокирован'); };
-            Lampa.Ad.load  = function() { console.log('[AdBlock] Ad.load заблокирован'); };
-            Lampa.Ad.get   = function() { return ''; };
-            Lampa.Ad.start = function() {};
-        }
 
-        if (Lampa.Ads) {
-            Lampa.Ads.show  = function() {};
-            Lampa.Ads.load  = function() {};
-            Lampa.Ads.get   = function() { return ''; };
-            Lampa.Ads.start = function() {};
-        }
+function clearAdTimers() {
+let highestTimeout = setTimeout(() => {}, 0);
+for (let i = 0; i <= highestTimeout; i++) {
+clearTimeout(i);
+clearInterval(i);
+}
+}
 
-        // ============================================
-        // 3. Подменяем статус аккаунта (премиум)
-        // ============================================
-        if (Lampa.Account) {
-            var origHasPremium = Lampa.Account.hasPremium;
-            Lampa.Account.hasPremium = function() { return true; };
 
-            // Некоторые версии используют .premium()
-            if (Lampa.Account.premium) {
-                Lampa.Account.premium = function() { return true; };
-            }
-        }
+document.addEventListener("DOMContentLoaded", clearAdTimers);
 
-        observer.observe(document.body, { childList: true, subtree: true });
-
-        console.log('[AdBlock] Блокировка рекламы активна ✓');
-    });
 })();
